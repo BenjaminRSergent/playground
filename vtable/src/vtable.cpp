fefe57d76ddd61080be790d5aa859862ecf1a247
Benjamin Sergent <benjamin.sergent@daqri.com>
2016-05-31 15:07:30 -0700

    Minor Correction

--- vtable/src/vtable.cpp
+++ vtable/src/vtable.cpp
@@ -3,7 +3,6 @@ bool globalChild;
 enum FunctionType {
 	PRINTA = 0,
 	PRINTB = 1,
-	OTHER = 3
 };
 
 FunctionType gobalFuncType;
@@ -11,14 +10,14 @@ FunctionType gobalFuncType;
 class BaseA {
 public:
 	virtual void printA() {
-		std::cout << "Inside " << __func__ << " this is " << ((!globalChild && gobalFuncType == PRINTA) ? "Correct" : "Incorrect");
+		std::cout << "Inside " << __func__ << " parent this is " << ((!globalChild && gobalFuncType == PRINTA) ? "Correct" : "Incorrect");
 	}
 };
 
 class BaseB {
 public:
 	virtual void printB() {
-		std::cout << "Inside " << __func__  <<" this is " << ((!globalChild && gobalFuncType == PRINTB) ? "Correct" : "Incorrect");
+		std::cout << "Inside " << __func__  <<" parent this is " << ((!globalChild && gobalFuncType == PRINTB) ? "Correct" : "Incorrect");
 	}
 };
 
@@ -26,30 +25,30 @@ public:
 class DoubleParent : public BaseA, public BaseB {
 public:
 	virtual void printChild() {
-		std::cout << "Inside " << __func__ << " this is " << ((globalChild && gobalFuncType == OTHER) ? "Correct" : "Incorrect");
+		std::cout << "Inside " << __func__ << " this is " << ((globalChild) ? "Correct" : "Incorrect");
 	}
 
 	virtual void printA() override {
-		std::cout << "Inside " << __func__ << " this is " << ((globalChild && gobalFuncType == OTHER) ? "Correct" : "Incorrect");
+		std::cout << "Inside " << __func__ << " child this is " << ((globalChild && gobalFuncType == PRINTA) ? "Correct" : "Incorrect");
 	}
 
 	virtual void printB() override{
-		std::cout << "Inside " << __func__ << " this is " << ((globalChild && gobalFuncType == OTHER) ? "Correct" : "Incorrect");
+		std::cout << "Inside " << __func__ << " child this is " << ((globalChild && gobalFuncType == PRINTB) ? "Correct" : "Incorrect");
 	}
 
 	virtual void printChildTwo() {
-		std::cout << "Inside " << __func__ << " this is " << ((globalChild && gobalFuncType == OTHER) ? "Correct" : "Incorrect");
+		std::cout << "Inside " << __func__ << " this is " << ((globalChild) ? "Correct" : "Incorrect");
 	}
 
 };
 
 typedef void (*testFunc)(void); 
-void callVoidOnVtable(uintptr_t* vtable, uintptr_t obj, int numFuncs, bool isChild) {
+void callVoidOnVtable(uintptr_t obj, int numFuncs) {
+	uintptr_t* vtable = *(uintptr_t**)obj;
 	uintptr_t tableLoc = reinterpret_cast<uintptr_t>(vtable);
 	std::cout << "The object is located at " << std::hex << obj << "\n";
 	std::cout << "The Vtable is located at " << std::hex << tableLoc << "\n";
 
-	globalChild = isChild;
 	for(int index = 0; index < numFuncs; index++) {
 		std::cout << "Calling the function at " << std::hex << vtable[index] << " which is index " << index << "\n";
 		testFunc func = reinterpret_cast<testFunc>(vtable[index]);
@@ -69,60 +68,81 @@ int main(int argc, char** argv) {
 	BaseA* reinterpretDoubleAsA = reinterpret_cast<BaseA*>(doubleParent);
 	BaseB* reinterpretDoubleAsB = reinterpret_cast<BaseB*>(doubleParent);
 
+	void* voidDouble = static_cast<void*>(doubleParent);
+
+	BaseA* voidAndBackA = static_cast<BaseA*>(voidDouble);
+	BaseB* voidAndBackB = static_cast<BaseB*>(voidDouble);
+
+
+	BaseA* reintVoidAndBackA = reinterpret_cast<BaseA*>(reinterpret_cast<void*>(doubleParent));
+	BaseB* reintVoidAndBackB = reinterpret_cast<BaseB*>(reinterpret_cast<void*>(doubleParent));
+
 	BaseA* actualA = new BaseA();
 	BaseB* actualB = new BaseB();
 
-	uintptr_t* doubleVtable = *(uintptr_t**)doubleParent;
-	uintptr_t* secondDoubleVtable = *(uintptr_t**)secondDoubleParent;
-	uintptr_t* baseAVtable = *(uintptr_t**)actualA;
-	uintptr_t* baseBVtable = *(uintptr_t**)actualB;
-
-
-	uintptr_t* staticCastBaseAVtable = *(uintptr_t**)staticDoubleAsA;
-	uintptr_t* staticCastBaseBVtable = *(uintptr_t**)staticDoubleAsB;
-
-	uintptr_t* reinterpretCastBaseAVtable = *(uintptr_t**)reinterpretDoubleAsA;
-	uintptr_t* reinterpretCastBaseBVtable = *(uintptr_t**)reinterpretDoubleAsB;
-
 
 	gobalFuncType = PRINTA;
+	globalChild = false;
 	std::cout << "Actual Base A vtable\n";
-	callVoidOnVtable(baseAVtable, reinterpret_cast<uintptr_t>(actualA), 1, false);
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(actualA), 1);
 
 	gobalFuncType = PRINTB;
 	std::cout << "============================\n";
 	std::cout << "Actual Base B vtable\n";
-	callVoidOnVtable(baseBVtable, reinterpret_cast<uintptr_t>(actualB), 1, false);
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(actualB), 1);
 
+
+	globalChild = true;
 	gobalFuncType = PRINTA;
 	std::cout << "============================\n";
 	std::cout << "static_cast DoubleParent to BaseA vtable\n";
-	callVoidOnVtable(staticCastBaseAVtable, reinterpret_cast<uintptr_t>(staticDoubleAsA), 1, true);
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(staticDoubleAsA), 1);
 
 	gobalFuncType = PRINTB;
 	std::cout << "============================\n";
 	std::cout << "static_cast DoubleParent to BaseB vtable\n";
-	callVoidOnVtable(staticCastBaseBVtable, reinterpret_cast<uintptr_t>(staticDoubleAsB), 1, true);
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(staticDoubleAsB), 1);
 
 	gobalFuncType = PRINTA;
 	std::cout << "============================\n";
 	std::cout << "reinterpret_cast DoubleParent to BaseA vtable\n";
-	callVoidOnVtable(reinterpretCastBaseAVtable, reinterpret_cast<uintptr_t>(reinterpretDoubleAsA), 1, true);
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(reinterpretDoubleAsA), 1);
 
 	gobalFuncType = PRINTB;
 	std::cout << "============================\n";
 	std::cout << "reinterpret_cast DoubleParent to BaseB vtable\n";
-	callVoidOnVtable(reinterpretCastBaseBVtable, reinterpret_cast<uintptr_t>(reinterpretDoubleAsB), 1, true);
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(reinterpretDoubleAsB), 1);
 
+	gobalFuncType = PRINTA;
+	std::cout << "============================\n";
+	std::cout << "static_cast void and back DoubleParent to BaseA vtable\n";
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(voidAndBackA), 1);
+
+	gobalFuncType = PRINTB;
+	std::cout << "============================\n";
+	std::cout << "static_cast void and back DoubleParent to BaseB vtable\n";
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(voidAndBackB), 1);
+
+	gobalFuncType = PRINTA;
+	std::cout << "============================\n";
+	std::cout << "reinterpret_cast void and back DoubleParent to BaseA vtable\n";
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(reintVoidAndBackA), 1);
+
+	gobalFuncType = PRINTB;
+	std::cout << "============================\n";
+	std::cout << "reinterpret_cast void and back DoubleParent to BaseB vtable\n";
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(reintVoidAndBackB), 1);
+
+/*
 	gobalFuncType = OTHER;
 	std::cout << "============================\n";
 	std::cout << "First DoubleParent vtable\n";
-	callVoidOnVtable(doubleVtable, reinterpret_cast<uintptr_t>(doubleParent), 4, true);
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(doubleParent), 4);
 
 	gobalFuncType = OTHER;
 	std::cout << "============================\n";
 	std::cout << "Second DoubleParent vtable\n";
-	callVoidOnVtable(secondDoubleVtable, reinterpret_cast<uintptr_t>(secondDoubleParent), 4, true);
-
+	callVoidOnVtable(reinterpret_cast<uintptr_t>(secondDoubleParent), 4);
+*/
 	return 0;
 }
